4304f9f17c1a27c1cdc590f97e4dc45a1005dea9
Ben Fitzhardinge <ben@fitzhardinge.net>
2020-01-22 11:09:19 +0800

    testing update

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 LOG_ID=$1
-./hook_compose_v1.py '{"url": "https://uat.riw.net.au/Sync/serviceApi/Breathalyser/RecordBreathalyserResult", "headers": {"Host": "uat.riw.net.au", "Authorization": "123"}, "table_name": "machine_log_wm4", "table_id": 3129266, "hook_log_id": '${LOG_ID}' }'
+./hook_compose_v1.py '{"url": "'${TEST_URL}'", "headers": {"Authorization": "'${TEST_AUTH}'"}, "table_name": "machine_log_wm4", "table_id": 3129266, "hook_log_id": '${LOG_ID}' }'
